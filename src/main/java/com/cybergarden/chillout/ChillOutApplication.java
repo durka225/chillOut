cc738bff5bfa4c114694b25141773fc6a4998bb8
durka225 <msviridov02@mail.ru>
2025-12-05 18:19:44 +0300

    Initial Project

---
+++ src/main/java/com/cybergarden/chillout/ChillOutApplication.java
@@ -0,0 +1,13 @@
+package com.cybergarden.chillout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChillOutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChillOutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cybergarden/chillout/ChillOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybergarden.chillout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChillOutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
