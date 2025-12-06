cbcbe4a8e61dd8b64998a79e4acad1f3d2ffbd75
NikitaRaze <nekit.nik1990@mail.ru>
2025-12-06 04:02:53 +0300

    NewBuyScreen

--- app/src/main/java/com/example/chillout/presentation/screen/login/LoginScreen.kt
+++ app/src/main/java/com/example/chillout/presentation/screen/login/LoginScreen.kt
@@ -27,16 +27,10 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
---
+++ app/src/main/java/com/example/chillout/presentation/screen/viewmodel/NewBuyScreenViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.chillout.presentation.screen.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class NewBuyScreenViewModel: ViewModel () {
+    var name by mutableStateOf("")
+        private set
+
+    var price by mutableStateOf("")
+        private set
+
+    var categoryName by mutableStateOf("")
+        private set
+
+
+    fun updateName(name: String){
+        this.name = name
+    }
+    fun updatePrice (price: String){
+        this.price = price
+    }
+    fun updateCategoryName(categoryName: String){
+        this.categoryName = categoryName
+    }
+}
